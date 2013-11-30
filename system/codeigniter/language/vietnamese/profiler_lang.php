2a84026d748ac3583b0a7417e6e373ad0c150e8a
nbao <lazzyvn@yahoo.com>
2013-11-30 17:46:48 +0100

    Update profiler_lang.php
    change lang vietnamese

--- system/codeigniter/language/vietnamese/profiler_lang.php
+++ system/codeigniter/language/vietnamese/profiler_lang.php
@@ -1,25 +1,25 @@
 <?php
 
-$lang['profiler_database']		= 'DATABASE';
-$lang['profiler_controller_info'] = 'CLASS/METHOD';
-$lang['profiler_benchmarks']	= 'BENCHMARKS';
-$lang['profiler_queries']		= 'QUERIES';
-$lang['profiler_get_data']		= 'GET DATA';
-$lang['profiler_post_data']		= 'POST DATA';
-$lang['profiler_uri_string']	= 'URI STRING';
-$lang['profiler_memory_usage']	= 'MEMORY USAGE';
-$lang['profiler_config']		= 'CONFIG VARIABLES';
+$lang['profiler_database']		= 'Cơ sở dử liệu';
+$lang['profiler_controller_info'] = 'Lớp/Phương thức';
+$lang['profiler_benchmarks']	= 'Đánh giá hiệu năng';
+$lang['profiler_queries']		= 'Truy vấn';
+$lang['profiler_get_data']		= 'Lấy dử liêu';
+$lang['profiler_post_data']		= 'Đăng dử liệu';
+$lang['profiler_uri_string']	= 'Chuổi URI';
+$lang['profiler_memory_usage']	= 'Bộ Nhớ Sử Dụng';
+$lang['profiler_config']		= 'Cấu Hình Biến';
 $lang['profiler_session_data']	= 'SESSION DATA';
 $lang['profiler_headers']		= 'HTTP HEADERS';
-$lang['profiler_no_db']			= 'Database driver is not currently loaded';
-$lang['profiler_no_queries']	= 'No queries were run';
-$lang['profiler_no_post']		= 'No POST data exists';
-$lang['profiler_no_get']		= 'No GET data exists';
-$lang['profiler_no_uri']		= 'No URI data exists';
-$lang['profiler_no_memory']		= 'Memory Usage Unavailable';
-$lang['profiler_no_profiles']	= 'No Profile data - all Profiler sections have been disabled.';
-$lang['profiler_section_hide']	= 'Hide';
-$lang['profiler_section_show']	= 'Show';
+$lang['profiler_no_db']			= 'Chưa tải trình điều khiển dữ liệu';
+$lang['profiler_no_queries']	= 'Không có truy vấn để chạy';
+$lang['profiler_no_post']		= 'Không tồn tại dử liệu POST';
+$lang['profiler_no_get']		= 'Không tồn tại dử liệu GET';
+$lang['profiler_no_uri']		= 'Không tồn tại dử liệu URI';
+$lang['profiler_no_memory']		= 'Bộ nhớ đã sử dụng không tồn tại';
+$lang['profiler_no_profiles']	= 'Không có Dử Liệu Profile - Tất cả Profiler đã bị vô hiệu hóa.';
+$lang['profiler_section_hide']	= 'Ẩn';
+$lang['profiler_section_show']	= 'Hiện';
 
 /* End of file profiler_lang.php */
 /* Location: ./system/language/english/profiler_lang.php */
